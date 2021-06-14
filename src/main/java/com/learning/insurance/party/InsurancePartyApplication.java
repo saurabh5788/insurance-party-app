590201025f90512803a561a27c706e3c661f232b
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-06-15 01:09:29 +0530

    Added Eureka Support

--- src/main/java/com/learning/insurance/party/InsurancePartyApplication.java
+++ src/main/java/com/learning/insurance/party/InsurancePartyApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class InsurancePartyApplication implements ApplicationRunner {
 	private static final Logger LOGGER = LoggerFactory
