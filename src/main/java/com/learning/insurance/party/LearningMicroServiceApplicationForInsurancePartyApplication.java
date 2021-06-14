a6b90a36a8aa18f99ffc26a6ec1aa0799b9dd0bb
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-06-14 22:42:37 +0530

    first commit

---
+++ src/main/java/com/learning/insurance/party/LearningMicroServiceApplicationForInsurancePartyApplication.java
@@ -0,0 +1,13 @@
+package com.learning.insurance.party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMicroServiceApplicationForInsurancePartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningMicroServiceApplicationForInsurancePartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/insurance/party/LearningMicroServiceApplicationForInsurancePartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.insurance.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningMicroServiceApplicationForInsurancePartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
